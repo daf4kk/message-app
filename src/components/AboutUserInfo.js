dc5b8a4a64563fa94760b34148fd86be5176d61d
daf4kk <104496507+daf4kk@users.noreply.github.com>
2022-08-16 22:12:11 +0300

    16.08.22
    Я такой дурак, проблема бы в деструктуризаций, всё таки решил доделать отображение друзей пользователя и сделал за 10 минут... а сидел 2 дня
    
    НО ! НУЖНО БУДЕТ АКТИВНОГО ПОЛЬЗОВАТЕЛЯ ДОБАВИТЬ В USERS, ПРАВДА НУЖНО УЧЕСТЬ КОЕ ЧТО САМ ВСПОНМЮ ОЙ КАПС А ЕЩё названия переменных поменять

--- src/components/AboutUserInfo.js
+++ src/components/AboutUserInfo.js
@@ -3,7 +3,6 @@ import './AboutUserInfo.css';
 import addFriendIcon from './HomePage/imgs/invite.png';
 function AboutUserInfo({neededUserId, users, activeUser, addOurRequests}){
 
-    const [friendsList1, setFriendsList1] = useState([]);
     const [da,setDa] = useState({...activeUser}); //Во, должно быть начальное значение потом он как бы считывает еффект и находит именно тот что нам нужен
     useEffect( () =>{
         //Надеюсь завтра быстро вспомню что к чему
@@ -21,6 +20,32 @@ function AboutUserInfo({neededUserId, users, activeUser, addOurRequests}){
     const {id, name, email, city, avatarSettings, friends} = da;
     // 3 или сколько там
     // так получается тут нужно друзей обрабатывать нет?
+    let list = [];
+    //Можно перебирать friends и fiend сразу в users
+    users.forEach((user) => {
+        friends.forEach((friendId) =>{
+            if (user.id === friendId){
+                list.push(user);
+            }
+        })
+    });
+    const renderedList = list.map((friendObj) => {
+        // const {frienId, friendName, friendAvatarSettings} = friendObj; //Неправильно !
+        //Через деструктуризацию по какой то причине просто напросто не работает (блять я такой дебил я же деструктуризирую ну через as какой нибудь поэтому и было undefined...)
+        //но если не так то как...
+        //Видимо только так как сделал я
+        return (
+            <>
+                <li className='about-user-friend' key = {`${friendObj.id}aboutFriend`}>
+                <div className='about-user-user-avatar' style = {{backgroundColor: `rgba(${friendObj.avatarSettings[1]})` }}>
+                    <p style = {{color: `rgba(${friendObj.avatarSettings[2]})`}}>{friendObj.avatarSettings[0]}</p>
+                </div>
+                    <p className = 'about-user-friend-name'>{friendObj.name}</p>
+                </li>
+
+            </>
+        )
+    });
     return (
         
         <div className='about-user-info'>
@@ -49,7 +74,7 @@ function AboutUserInfo({neededUserId, users, activeUser, addOurRequests}){
         </div>
         <p className='all-friends-title'>Друзья этого пользователя: </p>
         <div className='about-friends-list'>
-
+            {renderedList}
         </div>
     </div>
     );
